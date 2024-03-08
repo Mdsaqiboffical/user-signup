89d901b0ca459b0aa9799dcf93dcfa994f72e5c7
Mdsaqiboffical <154467672+Mdsaqiboffical@users.noreply.github.com>
2024-03-08 23:59:28 +0500

    done

---
+++ app.js
@@ -0,0 +1,11 @@
+var userName = prompt("Do You Want To SingUp (Yes/No)")
+// alert(userName)
+
+if (userName == "Yes") {
+    prompt("Enter Your Name!") &&
+        alert("SignUp Succeesfully")
+} else {
+    alert("Go Back")
+}
+Array [""]
+console.log
